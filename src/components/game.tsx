65bda54188b1749020309aa8dfa00b06c44ba690
snerks <martin.github@outlook.com>
2018-09-05 13:21:57 +0100

    Draft - working

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -64,6 +64,9 @@ export interface GameState {
   lives: number;
   score: number;
   scoreHigh: number;
+
+  text: string;
+  textAlpha: number;
 }
 
 const FireKeyCode = 32; // Space
@@ -76,6 +79,9 @@ class Game extends React.Component<GameProps, GameState> {
 
   private FPS: number = 60; // frames per second
   private TEXT_SIZE: number = 40;
+  private TEXT_FADE_TIME: number = 2.5; // text fade time in seconds
+
+  private GAME_LIVES: number = 3; // starting number of lives
 
   private FRICTION: number = 0.7; // friction coefficient of space (0 = no friction, 1 = lots of friction)
 
@@ -85,22 +91,24 @@ class Game extends React.Component<GameProps, GameState> {
   private SHIP_TURN_SPEED_DEGREES_SEC: number = 150; // turn speed in degrees per second
 
   private SHIP_BLINK_DUR: number = 0.1; // duration in seconds of a single blink during ship's invisibility
-  // private SHIP_EXPLODE_DUR: number  = 0.3; // duration of the ship's explosion in seconds
+  private SHIP_EXPLODE_DUR: number = 0.3; // duration of the ship's explosion in seconds
   private SHIP_INV_DUR: number = 3; // duration of the ship's invisibility in seconds
 
   private LASER_DISTANCE: number = 0.6; // max distance laser can travel as fraction of screen width
-  // private LASER_EXPLODE_DURATION_SEC: number = 0.1; // duration of the lasers' explosion in seconds
+  private LASER_EXPLODE_DURATION_SEC: number = 0.1; // duration of the lasers' explosion in seconds
   private LASER_MAX_COUNT: number = 10; // maximum number of lasers on screen at once
   private LASER_SPEED_PX_SEC: number = 500; // speed of lasers in pixels per second
 
   private ROID_JAG: number = 0.4; // jaggedness of the asteroids (0 = none, 1 = lots)
-  // private ROID_PTS_LGE: number = 20; // points scored for a large asteroid
-  // private ROID_PTS_MED: number = 50; // points scored for a medium asteroid
-  // private ROID_PTS_SML: number = 100; // points scored for a small asteroid
+  private ROID_PTS_LGE: number = 20; // points scored for a large asteroid
+  private ROID_PTS_MED: number = 50; // points scored for a medium asteroid
+  private ROID_PTS_SML: number = 100; // points scored for a small asteroid
   private ROID_MINIMUM_COUNT: number = 3; // starting number of asteroids
   private ROID_SIZE: number = 100; // starting size of asteroids in pixels
   private ROID_SPD: number = 50; // max starting speed of asteroids in pixels per second
   private ROID_VERT: number = 10; // average number of vertices on each asteroid
+
+  private SAVE_KEY_SCORE: string = "highscore"; // save key for local storage of high score
 
   private SHOW_BOUNDING = false; // show or hide collision bounding
   // private SHOW_CENTRE_DOT = false; // show or hide ship's centre dot
@@ -123,6 +131,9 @@ class Game extends React.Component<GameProps, GameState> {
     const { width, height } = props;
 
     this.state = {
+      text: "",
+      textAlpha: 1.0,
+
       levelIndex: 0,
 
       ship: {
@@ -155,7 +166,7 @@ class Game extends React.Component<GameProps, GameState> {
       roidsLeft: 0,
       roidsTotal: 0,
 
-      lives: 0,
+      lives: 3,
       score: 0,
       scoreHigh: 0
     };
@@ -169,11 +180,14 @@ class Game extends React.Component<GameProps, GameState> {
     // document.addEventListener("keydown", this.keyDown);
     // document.addEventListener("keyup", this.keyUp);
 
+    const nextState = { ...this.state };
+    this.newGame(nextState);
+
     // set up the game loop
     setInterval(() => this.updateGameStatus(), 1000 / this.FPS);
     // this.updateGameStatus();
 
-    this.createAsteroidBelt();
+    // this.createAsteroidBelt();
   }
 
   public componentDidUpdate() {
@@ -213,10 +227,10 @@ class Game extends React.Component<GameProps, GameState> {
     return roid;
   }
 
-  public createAsteroidBelt() {
+  public createAsteroidBelt(newState: GameState) {
     const { width, height } = this.props;
 
-    const newState = { ...this.state };
+    // const newState = { ...this.state };
 
     const { levelIndex, ship } = newState;
 
@@ -253,7 +267,7 @@ class Game extends React.Component<GameProps, GameState> {
     const nextState = { ...this.state };
 
     // handle Ship
-    const { ship, roids } = nextState;
+    const { ship, roids, textAlpha } = nextState;
 
     // rotate the ship
     ship.angleInRadians += ship.rotationInRadians;
@@ -322,6 +336,25 @@ class Game extends React.Component<GameProps, GameState> {
       // fxThrust.stop();
     }
 
+    // draw the triangular ship
+    if (!exploding) {
+      // if (blinkOn && !ship.dead) {
+      //   drawShip(ship.x, ship.y, ship.a);
+      // }
+
+      // handle blinking
+      if (ship.blinkNum > 0) {
+        // reduce the blink time
+        ship.blinkTime--;
+
+        // reduce the blink num
+        if (ship.blinkTime === 0) {
+          ship.blinkTime = Math.ceil(this.SHIP_BLINK_DUR * this.FPS);
+          ship.blinkNum--;
+        }
+      }
+    }
+
     // move the laser bullets
     for (let i = ship.lasers.length - 1; i >= 0; i--) {
       // check distance travelled
@@ -382,7 +415,199 @@ class Game extends React.Component<GameProps, GameState> {
       }
     }
 
+    // detect laser hits on asteroids
+    let ax: number;
+    let ay: number;
+    let ar: number;
+    let lx: number;
+    let ly: number;
+
+    for (let i = roids.length - 1; i >= 0; i--) {
+      // grab the asteroid properties
+      ax = roids[i].x;
+      ay = roids[i].y;
+      ar = roids[i].r;
+
+      // loop over the lasers
+      for (let j = ship.lasers.length - 1; j >= 0; j--) {
+        // grab the laser properties
+        lx = ship.lasers[j].x;
+        ly = ship.lasers[j].y;
+
+        // detect hits
+        if (
+          ship.lasers[j].explodeTime === 0 &&
+          this.distBetweenPoints(ax, ay, lx, ly) < ar
+        ) {
+          // destroy the asteroid and activate the laser explosion
+          this.destroyAsteroid(nextState, i);
+          ship.lasers[j].explodeTime = Math.ceil(
+            this.LASER_EXPLODE_DURATION_SEC * this.FPS
+          );
+          break;
+        }
+      }
+    }
+
+    // check for asteroid collisions (when not exploding)
+    if (!exploding) {
+      // console.log(`exploding = [${exploding}]`);
+
+      // only check when not blinking
+      if (ship.blinkNum === 0 && !ship.dead) {
+        // console.log(`Checking Ship/Asteroid collision`);
+
+        for (let i = 0; i < roids.length; i++) {
+          if (
+            this.distBetweenPoints(
+              ship.centreX,
+              ship.centreY,
+              roids[i].x,
+              roids[i].y
+            ) <
+            ship.radius + roids[i].r
+          ) {
+            this.explodeShip();
+            this.destroyAsteroid(nextState, i);
+            break;
+          }
+        }
+      }
+
+      // // rotate the ship
+      // ship.angleInRadians += ship.rotationInRadians;
+
+      // // move the ship
+      // ship.centreX += ship.thrust.x;
+      // ship.centreY += ship.thrust.y;
+    } else {
+      // reduce the explode time
+      ship.explodeTime--;
+
+      // reset the ship after the explosion has finished
+      if (ship.explodeTime === 0) {
+        nextState.lives--;
+        if (nextState.lives === 0) {
+          this.gameOver(nextState);
+        } else {
+          nextState.ship = this.newShip();
+        }
+      }
+    }
+
+    // draw the game text
+    if (textAlpha >= 0) {
+      // // console.log("draw the game text");
+      // ctx.textAlign = "center";
+      // ctx.textBaseline = "middle";
+      // ctx.fillStyle = "rgba(255, 255, 255, " + textAlpha + ")";
+      // ctx.font = "small-caps " + this.TEXT_SIZE + "px dejavu sans mono";
+      // ctx.fillText(text, this.props.width / 2, this.props.height * 0.75);
+      nextState.textAlpha -= 1.0 / this.TEXT_FADE_TIME / this.FPS;
+    } else if (ship.dead) {
+      // after "game over" fades, start a new game
+      this.newGame(nextState);
+    }
+
     this.setState(nextState);
+  }
+
+  public gameOver(nextState: GameState) {
+    // const nextState = { ...this.state };
+    const { ship } = nextState;
+
+    ship.dead = true;
+
+    nextState.text = "Game Over";
+    nextState.textAlpha = 1.0;
+  }
+
+  public newShip(): Ship {
+    const { width, height } = this.props;
+
+    return {
+      centreX: width / 2,
+      centreY: height / 2,
+
+      angleInRadians: (90 / 180) * Math.PI, // convert to radians
+      radius: this.SHIP_SIZE / 2,
+
+      blinkNum: Math.ceil(this.SHIP_INV_DUR / this.SHIP_BLINK_DUR),
+      blinkTime: Math.ceil(this.SHIP_BLINK_DUR * this.FPS),
+
+      canShoot: true,
+      dead: false,
+      explodeTime: 0,
+      lasers: [],
+      rotationInRadians: 0,
+      thrusting: false,
+
+      thrust: {
+        x: 0,
+        y: 0
+      }
+    };
+  }
+
+  public explodeShip() {
+    const nextState = { ...this.state };
+    const { ship } = nextState;
+
+    ship.explodeTime = Math.ceil(this.SHIP_EXPLODE_DUR * this.FPS);
+    // fxExplode.play();
+  }
+
+  public destroyAsteroid(nextState: GameState, index: number) {
+    console.log("destroyAsteroid : Start");
+
+    // const nextState = { ...this.state };
+
+    // handle Ship
+    const { roids } = nextState;
+
+    const x = roids[index].x;
+    const y = roids[index].y;
+    const r = roids[index].r;
+
+    // split the asteroid in two if necessary
+    if (r === Math.ceil(this.ROID_SIZE / 2)) {
+      // large asteroid
+      roids.push(this.getNewAsteroid(x, y, Math.ceil(this.ROID_SIZE / 4)));
+      roids.push(this.getNewAsteroid(x, y, Math.ceil(this.ROID_SIZE / 4)));
+      nextState.score += this.ROID_PTS_LGE;
+    } else if (r === Math.ceil(this.ROID_SIZE / 4)) {
+      // medium asteroid
+      roids.push(this.getNewAsteroid(x, y, Math.ceil(this.ROID_SIZE / 8)));
+      roids.push(this.getNewAsteroid(x, y, Math.ceil(this.ROID_SIZE / 8)));
+      nextState.score += this.ROID_PTS_MED;
+    } else {
+      nextState.score += this.ROID_PTS_SML;
+    }
+
+    // check high score
+    if (nextState.score > nextState.scoreHigh) {
+      nextState.scoreHigh = nextState.score;
+      localStorage.setItem(
+        this.SAVE_KEY_SCORE,
+        nextState.scoreHigh.toString(10)
+      );
+    }
+
+    // destroy the asteroid
+    roids.splice(index, 1);
+    // fxHit.play();
+
+    // calculate the ratio of remaining asteroids to determine music tempo
+    nextState.roidsLeft--;
+    // music.setAsteroidRatio(roidsLeft / roidsTotal);
+
+    // new level when no more asteroids
+    if (roids.length === 0) {
+      // nextState.levelIndex++;
+      this.newLevel(nextState);
+    }
+
+    console.log("destroyAsteroid : End");
   }
 
   public updateCanvas() {
@@ -396,40 +621,90 @@ class Game extends React.Component<GameProps, GameState> {
       return;
     }
 
+    const { score, scoreHigh } = this.state;
+
     // Draw background
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
+
+    // // draw the high score
+    // ctx.textAlign = "center";
+    // ctx.textBaseline = "middle";
+    // ctx.fillStyle = "white";
+    // ctx.font = this.TEXT_SIZE * 0.75 + "px dejavu sans mono";
+
+    // // ctx.fillText(
+    // //   `Level: ${this.state.levelIndex + 1}`,
+    // //   this.gameCanvas.width / 2,
+    // //   this.SHIP_SIZE
+    // // );
+
+    // // ctx.fillText(
+    // //   `Is Thrusting: ${this.state.ship.thrusting}`,
+    // //   this.gameCanvas.width / 2,
+    // //   this.SHIP_SIZE * 2
+    // // );
+
+    // // ctx.fillText(
+    // //   `Rotation: ${this.state.ship.rotationInRadians}`,
+    // //   this.gameCanvas.width / 2,
+    // //   this.SHIP_SIZE * 3
+    // // );
+
+    // ctx.fillText("BEST " + scoreHigh, this.props.width / 2, this.SHIP_SIZE);
+
+    // draw the score
+    ctx.textAlign = "right";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = "white";
+    ctx.font = this.TEXT_SIZE + "px dejavu sans mono";
+    ctx.fillText(
+      score.toString(10),
+      this.props.width - this.SHIP_SIZE / 2,
+      this.SHIP_SIZE
+    );
 
     // draw the high score
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillStyle = "white";
     ctx.font = this.TEXT_SIZE * 0.75 + "px dejavu sans mono";
-
-    // ctx.fillText("BEST " + scoreHigh, canv.width / 2, SHIP_SIZE);
-    ctx.fillText(
-      `Level: ${this.state.levelIndex + 1}`,
-      this.gameCanvas.width / 2,
-      this.SHIP_SIZE
-    );
-
-    ctx.fillText(
-      `Is Thrusting: ${this.state.ship.thrusting}`,
-      this.gameCanvas.width / 2,
-      this.SHIP_SIZE * 2
-    );
-
-    ctx.fillText(
-      `Rotation: ${this.state.ship.rotationInRadians}`,
-      this.gameCanvas.width / 2,
-      this.SHIP_SIZE * 3
-    );
+    ctx.fillText("BEST " + scoreHigh, this.props.width / 2, this.SHIP_SIZE);
 
     // Handle Ship
-    const { ship, roids } = this.state;
+    const { ship, roids, text, textAlpha, lives } = this.state;
+
+    // draw the game text
+    if (textAlpha >= 0) {
+      // console.log("draw the game text");
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillStyle = "rgba(255, 255, 255, " + textAlpha + ")";
+      ctx.font = "small-caps " + this.TEXT_SIZE + "px dejavu sans mono";
+      ctx.fillText(text, this.props.width / 2, this.props.height * 0.75);
+      // textAlpha -= 1.0 / TEXT_FADE_TIME / FPS;
+    } else if (ship.dead) {
+      // after "game over" fades, start a new game
+      // newGame();
+    }
 
     const blinkOn = ship.blinkNum % 2 === 0;
     const exploding = ship.explodeTime > 0;
+
+    // draw the lives
+    let lifeColour: string;
+
+    for (let i = 0; i < lives; i++) {
+      lifeColour = exploding && i === lives - 1 ? "red" : "white";
+
+      this.drawShip(
+        ctx,
+        this.SHIP_SIZE + i * this.SHIP_SIZE * 1.2,
+        this.SHIP_SIZE,
+        0.5 * Math.PI,
+        lifeColour
+      );
+    }
 
     if (ship.thrusting && !ship.dead) {
       // fxThrust.play();
@@ -477,7 +752,71 @@ class Game extends React.Component<GameProps, GameState> {
       // fxThrust.stop();
     }
 
-    this.drawShip(ctx, ship.centreX, ship.centreY, ship.angleInRadians);
+    if (!exploding) {
+      this.drawShip(ctx, ship.centreX, ship.centreY, ship.angleInRadians);
+    } else {
+      // draw the explosion (concentric circles of different colours)
+      ctx.fillStyle = "darkred";
+
+      ctx.beginPath();
+      ctx.arc(
+        ship.centreX,
+        ship.centreY,
+        ship.radius * 1.7,
+        0,
+        Math.PI * 2,
+        false
+      );
+      ctx.fill();
+
+      ctx.fillStyle = "red";
+      ctx.beginPath();
+      ctx.arc(
+        ship.centreX,
+        ship.centreY,
+        ship.radius * 1.4,
+        0,
+        Math.PI * 2,
+        false
+      );
+      ctx.fill();
+
+      ctx.fillStyle = "orange";
+      ctx.beginPath();
+      ctx.arc(
+        ship.centreX,
+        ship.centreY,
+        ship.radius * 1.1,
+        0,
+        Math.PI * 2,
+        false
+      );
+      ctx.fill();
+
+      ctx.fillStyle = "yellow";
+      ctx.beginPath();
+      ctx.arc(
+        ship.centreX,
+        ship.centreY,
+        ship.radius * 0.8,
+        0,
+        Math.PI * 2,
+        false
+      );
+      ctx.fill();
+
+      ctx.fillStyle = "white";
+      ctx.beginPath();
+      ctx.arc(
+        ship.centreX,
+        ship.centreY,
+        ship.radius * 0.5,
+        0,
+        Math.PI * 2,
+        false
+      );
+      ctx.fill();
+    }
 
     // draw the lasers
     for (let i = 0; i < ship.lasers.length; i++) {
@@ -767,6 +1106,36 @@ class Game extends React.Component<GameProps, GameState> {
     ctx.closePath();
     ctx.stroke();
   };
+
+  public newGame(newState: GameState) {
+    // const newState = { ...this.state };
+
+    newState.levelIndex = -1;
+    newState.lives = this.GAME_LIVES;
+    newState.score = 0;
+    newState.ship = this.newShip();
+
+    // get the high score from local storage
+    const scoreStr = localStorage.getItem(this.SAVE_KEY_SCORE);
+
+    if (scoreStr == null) {
+      newState.scoreHigh = 0;
+    } else {
+      newState.scoreHigh = parseInt(scoreStr, 10);
+    }
+
+    this.newLevel(newState);
+
+    // this.setState(newState);
+  }
+
+  public newLevel(newState: GameState) {
+    // music.setAsteroidRatio(1);
+    newState.levelIndex++;
+    newState.text = "Level " + (newState.levelIndex + 1);
+    newState.textAlpha = 1.0;
+    this.createAsteroidBelt(newState);
+  }
 }
 
 export default Game;
